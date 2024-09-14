30d525649f4c0014b69eed4d97e101eb30dbd08d
max_1103166 <1103166@hr.nl>
2024-09-14 17:39:33 +0200

    finished opdracht 5 2

---
+++ code/Opdracht_5_1.py
@@ -0,0 +1,11 @@
+numbers = list(range(1,31))
+
+for number in numbers:
+    if number % 3 == 0 and number % 5 == 0:
+        print("fizzbuzz")
+    elif number % 3 == 0:
+        print("fizz")
+    elif number % 5 == 0:
+         print("buzz")
+    else:
+        print(number)
